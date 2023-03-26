3a22a745e7d965afb5a41077a17c631c281ddb8b
Jiachen1121 <jackkie.m1121@gmail.com>
2023-03-27 00:05:47 +0800

    preprocessing

---
+++ Analysis/Exp1B.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(jsonlite)
+
+df <- read.csv("./rawdata_1B/exp1B_.csv")
+df$subj_idx <- jsonlite::fromJSON(df$response[5])$Q0
+df$gender <- jsonlite::fromJSON(df$response[6])
+df$year <- jsonlite::fromJSON(df$response[7])$Q0
+df$education <- jsonlite::fromJSON(df$response[8])$Q0
+df$dist <- df$view_dist_mm[9]
+df2 <- df %>% 
+  select(subj_idx, gender, year, education, dist, rt, correct_response, 
+         correct, key_press, Image, word, condition, trial_type) %>% 
+  dplyr::filter(trial_type == "psychophysics" & condition != "prac")
+
+df2$subj_idx <- as.numeric(df2$subj_idx)
+
+df3 <- df2 %>% 
+  mutate(
+    matchness =
+      case_when(
+        subj_idx %% 2 == 1 & correct_response == "j" ~ "match",
+        subj_idx %% 2 == 1 & correct_response == "f" ~ "mismatch", 
+        subj_idx %% 2 == 0 & correct_response == "f" ~ "match", 
+        subj_idx %% 2 == 0 & correct_response == "j" ~ "mismatch" 
+      )
+  )
+df3 %>% 
+  count(Image, matchness)
+df3$rt <- as.numeric(df3$rt)
+df3 <- df3 %>%
+  dplyr::filter(Image != "img/T_ambi40.png") #过滤掉填充试次
+df4 <- df3 %>%
+  select(rt,matchness) %>%
+  group_by(matchness) %>%
+  summarise(n = n(),
+            rt_avg = mean(rt, na.rm = TRUE)
+            ) %>%
+  ungroup()
+
