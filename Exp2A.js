bde9b5d1479cb91d65a0edf4c05a8d9ba79bc521
Jiachen1121 <jackkie.m1121@gmail.com>
2023-04-22 14:50:21 +0800

    Update practice instruction

--- Exp2A.js
+++ Exp2A.js
@@ -216,7 +216,7 @@ var welcome = {
       view_texts_images.forEach(v => {
         tmpI += `<p class="content">${v}</p>`;
       });
-      return ["<p class='header' style = 'font-size: 25px'>实验说明：</p><p style='color:white; font-size: 25px;line-height: 30px;'>您好，欢迎参加本实验。本次实验大约需要50分钟完成。</p><p style='color:white; font-size: 25px;'>在本实验中，您需要完成一个简单的知觉匹配任务。</p><p style='color:white; font-size: 25px;'>您将学习三种几何图形与文字标签的对应关系。</p>",
+      return ["<p class='header' style = 'font-size: 25px'>实验说明：</p><p style='color:white; font-size: 25px;line-height: 30px;'>您好，欢迎参加本实验。本次实验大约需要70分钟完成。</p><p style='color:white; font-size: 25px;'>在本实验中，您需要完成一个简单的知觉匹配任务。</p><p style='color:white; font-size: 25px;'>您将学习三种几何图形与文字标签的对应关系。</p>",
         start + `<div class="box">${tmpI}</div>` +
         `<p class='footer' style='font-size: 30px; line-height: 35px;'>您的任务是判断出现的图形是否与文字标签匹配。
         <p class='footer' style='font-size: 30px; line-height: 35px;'>每个阶段开始前，会指定一种需要您重点关注的图形。重点图形对应一种按键规则，非重点图形对应另一种按键规则。
@@ -227,7 +227,7 @@ var welcome = {
         <p class='footer' style='color:white; font-size: 30px;line-height: 35px;'>如果图形与下方文字匹配，请按<span style="color: lightgreen; font-size:35px"> ${key_2[0]} 键</span></p>
         <p class='footer' style='color:white; font-size: 30px;'>如果 不匹配，请按 <span style="color: lightgreen; font-size:35px">${key_2[1]}键</span></p>
         <p class='footer' style='color:white; font-size: 25px;'>请在实验过程中将您左右手的<span style="color: lightgreen;">食指和中指</span>放在电脑键盘的相应键位上准备按键。</p></span>`,
-        `<p style='color:white; font-size: 30px; line-height: 25px;'>您将首先完成三种要求下的练习试次。</p><p style='color:white; font-size: 30px; line-height: 30px;'>练习正确率达标后，您将完成三种要求下各5组匹配任务，每组包括72次按键反应，每组完成后会有休息时间。</p>`,
+        `<p style='color:white; font-size: 30px; line-height: 25px;'>您将首先完成三种要求下的自由练习和正式练习。</p><p style='color:white; font-size: 30px; line-height: 30px;'>练习正确率达标后，您将完成三种要求下各5组匹配任务，每组包括72次按键反应，每组完成后会有休息时间。</p>`,
         middle + end];
     },
     show_clickable_nav: true,
@@ -240,13 +240,14 @@ var welcome = {
     type: jsPsychInstructions,
     pages: function () {
         let start = "<p class='header' style = 'font-size: 25px'>任务要求：</p>",
-          end = "<p style = 'font-size: 25px; line-height: 30px;'>本阶段为自由练习，帮助您学习任务规则。如果您明白了规则：请点击 继续 </p><div>";
+          end = "<p style = 'font-size: 25px; line-height: 30px;'自由练习阶段目的是帮助您熟悉任务规则。如果您明白了规则：请点击 继续 </p><div>";
         return [
           start + 
           `<p class='footer' style='font-size: 30px; line-height: 35px;'>本阶段您需要重点关注与<span style="color: lightgreen; font-size:30px">“自我”对应的图形</span></p>
-          </p><p class='footer' style='color:white; font-size: 35px;'>如果出现的图形是与“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[1]}键</p></span>
-          </p><p class='footer' style='color:white; font-size: 35px;'>如果出现的图形不是“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[1]}键</p></span>
-          <p class='footer' style='color:white; font-size: 30px;'>请在实验过程中将您的<span style="color: lightgreen;">食指和中指</span>放在电脑键盘的相应键位上准备按键。</p></span>`
+          <p class='footer' style='color:white; font-size: 35px;'>如果出现的图形是与“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[1]}键</p></span>
+          <p class='footer' style='color:white; font-size: 35px;'>如果出现的图形不是“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[1]}键</p></span>
+          <p class='footer' style='color:white; font-size: 30px;'>请在实验过程中将您的<span style="color: lightgreen;">食指和中指</span>放在电脑键盘的相应键位上准备按键。</p></span>
+          <p class='footer' style='color:lightgreen; font-size: 30px;'>>本阶段为自由练习，不限定反应时间，请您尽可能正确地按键。`
           + end];
       },
     show_clickable_nav: true,
@@ -1005,13 +1006,14 @@ var welcome = {
     type: jsPsychInstructions,
     pages: function () {
         let start = "<p class='header' style = 'font-size: 25px'>任务要求：</p>",
-          end = "<p style = 'font-size: 25px; line-height: 30px;'>本阶段为正式练习，需要您在1.5s内尽量快速且准确地作出按键反应。如果您明白了规则：请点击 继续 </p><div>";
+          end = "<p style = 'font-size: 25px; line-height: 30px;'>如果您明白了规则：请点击 继续 </p><div>";
         return [
           start + 
           `<p class='footer' style='font-size: 30px; line-height: 35px;'>本阶段您需要重点关注与<span style="color: lightgreen; font-size:30px">“自我”对应的图形</span></p>
           </p><p class='footer' style='color:white; font-size: 35px;'>如果出现的图形是与“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_1[1]}键</p></span>
           </p><p class='footer' style='color:white; font-size: 35px;'>如果出现的图形不是“自我”对应的图形 且 该图形与下方的文字标签匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[0]}键</span></p><p class='footer' style='color:white; font-size: 35px;'>如果该图形与下方文字不匹配，请按<span style="color: lightgreen; font-size:35px">${key_2[1]}键</p></span>
-          <p class='footer' style='color:white; font-size: 30px;'>请在实验过程中将您的<span style="color: lightgreen;">食指和中指</span>放在电脑键盘的相应键位上准备按键。</p></span>`
+          <p class='footer' style='color:white; font-size: 30px;'>请在实验过程中将您的<span style="color: lightgreen;">食指和中指</span>放在电脑键盘的相应键位上准备按键。</p></span>
+          <p class='footer' style='color:lightgreen; font-size: 30px;'>>本阶段为正式练习，需要您在1.5s内尽量快速且准确地作出按键反应。`
           + end];
       },
     show_clickable_nav: true,
